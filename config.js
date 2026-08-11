dbd2261f2dd69a175cee7d8a63e1e8628dc36db1
ImSageforTay <sagekidd2@gmail.com>
2026-08-11 19:00:02 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Put your Supabase Project URL and browser-safe Publishable key here.
+// NEVER put a secret/service_role key in this file.
+window.SAGE_CONFIG = {
+  SUPABASE_URL: "https://jtbkxoaswpfnxnbyuzss.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_vogRuKzlTp1VDeDg3DjR1w_JknbvREr".};
